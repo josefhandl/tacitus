cdbdcc1b235611ccbf4f7be649acbba0579c77a8
Josef Handl <handljosef@gmail.com>
2023-12-18 17:37:48 +0000

    Refactor 'smarctl' endpoint to 'drives' endpoint, improves exceptions for smarctl

--- api/modules/drives/router.py
+++ api/modules/drives/router.py
@@ -1,6 +1,6 @@
 from .._model_base.router import BaseRouter
 from .input_models import SmartctlRoot, LsblkRoot, BlockDevice
-from .output_models import SmartResult, DriveInfo
+from .output_models import DrivesResult, DriveInfo
 from ...utilities.terminal import run_cmd
 import json
 from pydantic import ValidationError
@@ -10,34 +10,37 @@ import re
 from fastapi import HTTPException
 
 
-class Smartctl(BaseRouter):
-    PREFIX = "/smartctl"
+class Drives(BaseRouter):
+    PREFIX = "/drives"
 
-    async def get_root(self) -> SmartResult:
+    async def get_root(self) -> DrivesResult:
         # (lsblk) get all block devices
         lsblk_json = run_cmd("lsblk --json --output NAME,TYPE,MOUNTPOINT --tree")
         if not lsblk_json:
-            logger.error(f"Cannot get block devices")
-            raise HTTPException(status_code=500, detail="Cannot get block devices")
+            msg = f"Cannot get block devices"
+            logger.error(msg)
+            raise HTTPException(status_code=500, detail=msg)
 
-        # fill models with json
+        # Fill models with json
         try:
             lsblk_data = LsblkRoot.model_validate(json.loads(lsblk_json.stdout.decode()))
         except ValidationError as ex:
-            logger.error(f"Validation the lsblk failed. Exception: {ex}")
-            raise HTTPException(status_code=500, detail="Validation the lsblk failed")
+            msg = f"Validation the lsblk failed. Exception: {ex}"
+            logger.error(msg)
+            raise HTTPException(status_code=500, detail=msg)
         except Exception as ex:
-            logger.error(f"Unknown error. Exception: {ex}")
-            raise HTTPException(status_code=500, detail="Unknown error")
+            msg = f"Unknown error. Exception: {ex}"
+            logger.error(msg)
+            raise HTTPException(status_code=500, detail=msg)
 
-        # filter block devices to find only physical disks
+        # Filter block devices to find only physical disks
         disks: List[BlockDevice] = list()
         for bd in lsblk_data.blockdevices:
             if bd.type == "disk":
                 disks.append(bd)
 
         # (hdparm) get power mode but avoid to wake up the drives
-        # it is also possible to use smartctl, but it requires to know device type to avoid wake up
+        # It is also possible to use smartctl, but it requires to know device type to avoid wake up
         # (e.g. USB drives are problematic)
         power_modes = dict()
         for d in disks:
@@ -53,24 +56,28 @@ class Smartctl(BaseRouter):
 
             power_modes[d.name] = power_mode
 
-        # get health from all of the physical disks
+        # (smartctl) get health from all of the physical disks
         smarts: Dict[SmartctlRoot] = dict()
         for d in disks:
             smartctl_json = run_cmd(f"api/wrappers/w_smartctl /dev/{d.name}", [0, 4, 64, 68, 128])  # -d sat
             if not smartctl_json:
+                logger.warning(f"Failed to obtain S.M.A.R.T. info from /dev/{d.name} disk")
                 continue
 
+            # Fill models from smartctl output
             try:
                 smartctl_data = SmartctlRoot.model_validate(json.loads(smartctl_json.stdout.decode()))
                 smarts[d.name] = smartctl_data
             except ValidationError as ex:
-                logger.error(f"Validation the smartctl failed with /dev/{d.name} disk. Exception: {ex}")
-                raise HTTPException(status_code=500, detail="Validation the smartctl failed")
+                msg = f"Validation the smartctl failed with /dev/{d.name} disk. Exception: {ex}"
+                logger.error(msg)
+                raise HTTPException(status_code=500, detail=msg)
             except Exception as ex:
-                logger.error(ex)
-                raise HTTPException(status_code=500, detail="Unknown error")
+                msg = f"Unknown error. Exception: {ex}"
+                logger.error(msg)
+                raise HTTPException(status_code=500, detail=msg)
 
-        # convert Lsblk models, power modes and Smartctl models into the final model ready for output
+        # convert Lsblk models, power modes and smartctl models into the final model ready for output
         result: List[DriveInfo] = list()
         for physical_disk in disks:
             block_device_path = physical_disk.name
@@ -96,4 +103,4 @@ class Smartctl(BaseRouter):
         return {"result": [json.loads(di.model_dump_json()) for di in result]}
 
 
-router = Smartctl()
+router = Drives()
