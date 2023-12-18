f863b707ffdf0ce25603649fd2cbc81e356a7e58
Jan Janovec <janovec.j@outlook.cz>
2023-12-18 15:07:54 +0000

    Adding service

--- run_api.sh
+++ run_api.sh
@@ -1,2 +1,6 @@
-venv/bin/python -m uvicorn api.main:app --reload --host=0.0.0.0 --port=8000
+#!/bin/bash
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
+
+venv/bin/python -m uvicorn api.main:app --reload --host=0.0.0.0 --port=8155
 
