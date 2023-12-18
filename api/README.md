524e9224f37f4c0104b7f07d088795ef4f1c7a37
Jan Janovec <janovec.j@outlook.cz>
2023-12-18 00:14:46 +0000

    Black and more docs.

--- api/README.md
+++ api/README.md
@@ -1,5 +1,9 @@
 ## Run API
 Start by `python -m uvicorn api.main:app --reload --host=0.0.0.0 --port=8000` in VENV and go to `http://127.0.0.1:8000/docs`.
 
+### Ignoring modules
+Use `TACITUS_IGNORE_MODULES` environment variable to ignore modules. For example, to ignore `power` and `smartctl` modules, run:
+`TACITUS_IGNORE_MODULES=power,smartctl ./run_api.sh `
+
 ### Formating
 `black api -l 120`
--- api/main.py
+++ api/main.py
@@ -7,8 +7,9 @@ from os import environ
 def get_modules():
     modules_path = Path(__file__).parent / "modules"
     for module in modules_path.iterdir():
-        if module.is_dir() and module.name[0] != '_':
+        if module.is_dir() and module.name[0] != "_":
             yield module
+
 
 def include_modules_by_paths(module_paths: list, app):
     for module_path in module_paths:
@@ -19,19 +20,16 @@ def include_modules_by_paths(module_paths: list, app):
 
 
 if __name__ == "api.main":
-
     ignored_modules = environ.get("TACITUS_IGNORE_MODULES")
 
     app = FastAPI()
+
     @app.get("/")
     async def root() -> dict:
-        return {
-            "app": "tacitus-api",
-            "version": "0.1.1"
-        }
+        return {"app": "tacitus-api", "version": "0.1.1"}
 
     modules = get_modules()
     if ignored_modules:
-        ignored_modules = ignored_modules.split(',')
+        ignored_modules = ignored_modules.split(",")
         modules = filter(lambda module: module.name not in ignored_modules, modules)
     include_modules_by_paths(modules, app)
